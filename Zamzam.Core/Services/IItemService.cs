8cbdd0c0003d18987a28f8e7790b8253dcda0aca
MostafaOsman <abomariam10000@gmail.com>
2023-07-21 13:52:13 +0300

    add IItemServices

---
+++ Zamzam.Core/Services/IItemService.cs
@@ -0,0 +1,9 @@
+﻿namespace Zamzam.Core.Services
+{
+    public interface IItemService
+    {
+        Task<Item> GetItemAsync(int id);
+        Task DeleteItemAsync(int id);
+        Task<ICollection<Item>> GetItemsAsync();
+    }
+}
