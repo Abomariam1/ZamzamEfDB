b40417df5c9dafc897d77f3be62c1f0b5e57c0f4
MostafaOsman <abomariam10000@gmail.com>
2023-07-29 20:16:44 +0300

    Design Customers and Employee Views

--- Zamzam.Core/Entites/Customer.cs
+++ Zamzam.Core/Entites/Customer.cs
@@ -3,9 +3,9 @@
     public class Customer : BaseEntity
     {
         public string Name { get; set; } = "عميل افتراضي";
-        public long NationalCardId { get; set; } = 0;
         public string? Phone { get; set; } = string.Empty;
         public string? Address { get; set; } = string.Empty;
+        public long NationalCardId { get; set; } = 0;
         public string? Notes { get; set; } = string.Empty;
         public Ulid AreaId { get; set; }
         public bool IsProplem { get; set; } = false;
